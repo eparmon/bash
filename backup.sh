ec2f665ca772d3910235e273a7261a21e5e2c2de
Gene <eparmon@tuta.io>
2022-08-18 15:30:40 +0400

    Updated backup script so that it works for any user

--- backup.sh
+++ backup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-tar -czf /tmp/my_home_directory.tar.gz /home/gene
+# This bash script is used to backup a user's home directory to /tmp/.
+
+user=$(whoami)
+input=/home/$user
+output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
+
+tar -czf $output $input
+echo "Backup of $input completed! Details about the output backup file:"
+ls -l $output
